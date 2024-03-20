02c342e023d80be812063308837eda6f1da5dd9d
cipheryogi <137915333+cipheryogi@users.noreply.github.com>
2024-03-20 23:39:21 +0530

    Update Servicenow_GET_Incidents.py

--- Practice/Servicenow_GET_Incidents.py
+++ Practice/Servicenow_GET_Incidents.py
@@ -3,7 +3,7 @@ import openpyxl
 
 # url = "https://demotechnologywf38425.service-now.com/api/now/table/incident"
 url = "https://yshinde.service-now.com/api/now/table/incident"
-response = requests.get(url, auth=("admin", "Yogi4091!"))
+response = requests.get(url, auth=("admin", "password!"))
 
 incidents = response.json().get('result')
 print(incidents)
