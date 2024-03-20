a84a290a048cb85fa11b79418b566c4ba260237a
cipheryogi <137915333+cipheryogi@users.noreply.github.com>
2024-03-20 23:39:41 +0530

    Update Servicenow_Get_Incidents1.py

--- Practice/Servicenow_Get_Incidents1.py
+++ Practice/Servicenow_Get_Incidents1.py
@@ -3,7 +3,7 @@ import openpyxl
 
 # url = "https://demotechnologywf38425.service-now.com/api/now/table/incident"
 url = "https://yshinde.service-now.com/api/now/table/incident"
-response = requests.get(url, auth=("admin", "Yogi4091!"))
+response = requests.get(url, auth=("admin", "password"))
 try:
     if response.status_code == 200:
         incidents = response.json().get("result")
